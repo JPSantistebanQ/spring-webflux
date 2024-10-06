8cc84a14ad2e186bbb04573ce6d77e2018571c51
Juan Piero Santisteban Quiroz <jpsantq@gmail.com>
2024-10-05 22:29:13 -0500

    feat: add exchange filter in request with WebClient.

---
+++ src/test/java/com/udemy/webflux_playground/tests/sec07/Lec08ExchangeFilterTest.java
@@ -0,0 +1,47 @@
+package com.udemy.webflux_playground.tests.sec07;
+
+import com.udemy.webflux_playground.tests.sec07.dto.Product;
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Test;
+import org.springframework.web.reactive.function.client.ClientRequest;
+import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.test.StepVerifier;
+
+import java.util.UUID;
+
+@Log4j2
+class Lec08ExchangeFilterTest extends AbstractWebClient {
+    private final WebClient client = createWebClient(b -> b.filter(tokenGenerator()).filter(logHeaders()));
+
+    @Test
+    void exchangeFilter() {
+        this.client.get()
+                .uri("/lec09/product/{id}", 1)
+                .retrieve()
+                .bodyToMono(Product.class)
+                //.take(Duration.ofSeconds(3))
+                .doOnNext(print())
+                .then()
+                .as(StepVerifier::create)
+                .expectComplete()
+                .verify();
+    }
+
+    private ExchangeFilterFunction tokenGenerator() {
+        return ((request, next) -> {
+            String token = UUID.randomUUID().toString().replace("-", "");
+            log.info("Token generated: {}", token);
+            ClientRequest newRequest = ClientRequest.from(request).headers(httpHeaders -> httpHeaders.setBearerAuth(token)).build();
+            return next.exchange(newRequest);
+        });
+    }
+
+    private ExchangeFilterFunction logHeaders() {
+        return ((request, next) -> {
+            log.info("Method: {}", request.method());
+            log.info("URI: {}", request.url());
+            return next.exchange(request);
+        });
+    }
+}
